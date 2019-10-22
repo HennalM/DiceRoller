65fe80bd67986d5023bc2c912fcd2bbc762e67f0
HennalM <hennal_16@hotmail.com>
2019-10-22 16:52:50 +0100

    Added to code

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -11,16 +11,38 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
+//import static com.example./User/hennalmakwana/AndroidStudioProjext/.DiceRoller.R.id.numberEntered;
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mgTV;
+    private EditText numberEntered;
+    private Button validate;
+    private Object numberToFind;
+    private int counter = 0;
+    private TextView base;
+    private TextView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mgTV = (TextView) findViewById(R.id.HelloWorld);
+        numberEntered = (EditText) findViewById(R.id.numberEntered);
+        base = (TextView) findViewById(R.id.count);
+        view = (TextView) findViewById(R.id.counter);
+
+
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -56,15 +78,28 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void on_button_click(View view){
-        TextView tv = this.findViewById(R.id.RollDice);
-                Random r = new Random();
-        int number = r.nextInt(6) +1;
-        try {
-            tv.setText(number);
-        }catch (Exception ex) {
-            tv.setText(ex.toString());
+    public void on_button_click(View view)
+    {
+        TextView tv = (TextView) this.findViewById(R.id.RollDice);
+
+        Random r = new Random();
+
+        int number = r.nextInt(6);
+
+        tv.setText(Integer.toString(number));
+
+        int n = Integer.parseInt(numberEntered.getText().toString());
+
+        if (n < 1 || n > 6)
+        {
+            Toast.makeText(this, "Invalid input, the number must be between 1 - 6", Toast.LENGTH_SHORT).show();
+        }
+        else if (n == number)
+        {
+            Toast.makeText(this, "CONGRATULATION!, The number is a match!", Toast.LENGTH_SHORT).show();
+            counter = counter  +1;
+            base.setText(Integer.toString(counter));
         }
     }
-}
 
+}
